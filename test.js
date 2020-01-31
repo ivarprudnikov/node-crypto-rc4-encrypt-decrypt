7cb680a9c21b64b058f38f26db56046ca3317914
ivarprudnikov <ivar.prudnikov@gmail.com>
2020-01-31 23:26:10 +0000

    use assert in test and trivial tweaks in package.json

--- test.js
+++ test.js
@@ -1,17 +1,19 @@
 const encrypt = require('./encrypt');
 const decrypt = require('./decrypt');
+const assert = require('assert');
 
 const test = "foo bar baz";
 console.log(`encrypting ${test}`);
 encrypt.encryptStringAsync(test)
 	.then(details => {
 		console.log(`encrypted val ${details[1]}, encrypted key ${details[0]}`);
-		return details;
+		return details;		
 	})
 	.then(details => {
-		return decrypt.decryptWithEncryptedKey(details[0], details[1])
+		return decrypt.decryptWithEncryptedKey(details[0], details[1]);
 	})
+	.catch(err => assert.fail(`Unexpected error ${err}`))
 	.then(decrypted => {
-		console.log(`decrypted ${decrypted}`)
-	})
-
+		console.log(`decrypted ${decrypted}`);
+		assert.strictEqual(decrypted, "foo bar baz");
+	});
